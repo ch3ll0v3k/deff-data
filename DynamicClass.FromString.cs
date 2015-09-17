b83868907592f9e55a466e24eadfbb1677352b91
Timoschenko Viacheslau <avidon.ru@yandex.com>
2015-09-18 01:19:30 +0200

    Update DynamicClass.FromString.cs

--- DynamicClass.FromString.cs
+++ DynamicClass.FromString.cs
@@ -1,15 +1,19 @@
 
-
-Its pretty simple. Assume that your classname is Car and the namespace is Vehicles, then pass the parameter as Vehicles.Car which returns object of type Car. Like this you can create any instance of any class dynamically.
-
+/*
+Its pretty simple. Assume that your classname is Car and the namespace is Vehicles, 
+then pass the parameter as Vehicles.Car which returns object of type Car. Like this you 
+can create any instance of any class dynamically.
+*/
 public object GetInstance(string strFullyQualifiedName)
 {         
      Type t = Type.GetType(strFullyQualifiedName); 
      return  Activator.CreateInstance(t);         
 }
-
-If your Fully Qualified Name(ie, Vehicles.Car in this case) is in another assembly, the Type.GetType will be null. In such cases, you have loop through all assemblies and find the Type. For that you can use the below code
-
+/*
+If your Fully Qualified Name(ie, Vehicles.Car in this case) is in another assembly, 
+the Type.GetType will be null. In such cases, you have loop through all assemblies 
+and find the Type. For that you can use the below code
+*/
 public object GetInstance(string strFullyQualifiedName)
 {
      Type type = Type.GetType(strFullyQualifiedName);
@@ -24,11 +28,11 @@ public object GetInstance(string strFullyQualifiedName)
      return null;
  }
 
-Now if you want to call a parameterized constructor do the following
+//ow if you want to call a parameterized constructor do the following
 
 Activator.CreateInstance(t,17); // Incase you are calling a constructor of int type
 
-instead of
+//instead of
 
 Activator.CreateInstance(t);
 
